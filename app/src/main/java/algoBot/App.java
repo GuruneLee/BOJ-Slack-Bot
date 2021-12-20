e66da59a554cebe00e5dd60020302ca84cfa8c9e
gurunelee <dlckdgk4858@gmail.com>
2021-12-20 13:19:27 +0900

    feat : add getGreeting() method

--- app/src/main/java/algoBot/App.java
+++ app/src/main/java/algoBot/App.java
@@ -5,7 +5,11 @@ package algoBot;
 import algoBot.timer.Scheduler;
 
 public class App {
+    public String getGreeting() {
+        return "알고리즘을 푸는 게으름뱅이들을 위한 EkJoo 봇을 시작합니다";
+    }
     public static void main(String[] args) {
+        new App().getGreeting();
         new Scheduler().schedule();
     }
 }
